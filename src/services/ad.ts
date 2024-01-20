55fec4db7b0e61e3b7707d6487f8f61e1eb89643
韭皇啦 <51317376+zyancn@users.noreply.github.com>
2024-01-20 20:25:38 +0800

    Update ad.ts

--- src/services/ad.ts
+++ src/services/ad.ts
@@ -8,7 +8,8 @@ interface Ad {
 export const useAd = () => {
   const { data } = useSWR("images_ad", () => {
     return fetch(
-      "/",
+      "<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6400746346592229"
+     crossorigin="anonymous"></script>",
     )
       .then((res) => res.json())
       .then((res) => res.ad as Ad);
